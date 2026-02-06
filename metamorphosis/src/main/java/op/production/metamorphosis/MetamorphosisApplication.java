5a2d927c43b04db0b6ca19707ce9dd4ecdadceb1
Adam Lindström <adam.lindstrom@omegapoint.se>
2026-02-06 14:39:56 +0100

    feat: add generated spring project

---
+++ metamorphosis/src/main/java/op/production/metamorphosis/MetamorphosisApplication.java
@@ -0,0 +1,13 @@
+package op.production.metamorphosis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetamorphosisApplication {
+
+	static void main(String[] args) {
+		SpringApplication.run(MetamorphosisApplication.class, args);
+	}
+
+}
---
+++ metamorphosis/src/test/java/op/production/metamorphosis/MetamorphosisApplicationTests.java
@@ -0,0 +1,13 @@
+package op.production.metamorphosis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetamorphosisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
